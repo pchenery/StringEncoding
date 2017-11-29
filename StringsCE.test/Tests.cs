e4b7dca64d792739e88085d4915b3440268a83e1
Peter Chenery <peter.chenery@anthonynolan.org>
2017-11-29 19:50:43 +0000

    Added file identical test

---
+++ StringsCE.test/Tests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using StringsCE;
+using FluentAssertions;
+using System.IO;
+
+namespace StringsCE.tests
+{
+    [TestFixture]
+    public class StringFileTest
+    {
+
+        string originalfile = @"C:\Work\Training\StringsCE\originalbooks.dat";
+        string createdfile = @"C:\Work\Training\StringsCE\books.dat";
+
+        [Test]
+        public void FileShouldBeIdentical()
+        {
+            FileAssert.AreEqual(originalfile, createdfile);
+        }
+    }
+}
--- StringsCE/Book.cs
+++ StringsCE/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StringsCE
 {
-    class Book
+    public class Book
     {
         private string title;
         private string authors;
