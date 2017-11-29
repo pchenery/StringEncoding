54602e2ce4fc16b8b90529a259f41e5255a6358a
Peter Chenery <peter.chenery@anthonynolan.org>
2017-11-29 16:10:29 +0000

    Create new format file

---
+++ StringsCE/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsCE
+{
+    class Book
+    {
+        private string title;
+        private string authors;
+
+        const int maxTitleLen = 27;
+        const int maxAuthorsLen = 31;
+
+        public DateTime Published { get; set; }
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value.Length > maxTitleLen)
+                {
+                    title = value.Substring(0, maxTitleLen - 3) + "...";
+                }
+                else
+                {
+                    title = value;
+                }
+            }
+        }
+
+        public string Authors
+        {
+            get { return authors; }
+            set
+            {
+                if (value.Length > maxAuthorsLen)
+                {
+                    authors = value.Substring(0, maxAuthorsLen - 3) + "...";
+                }
+                else
+                {
+                    authors = value;
+                }
+            }
+        }
+    }
+}
---
+++ StringsCE/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsCE
+{
+    class Program
+    {
+        static Book book = new Book();
+        static List<Book> books = new List<Book>();
+
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"c:\work\training\stringsce\books.txt"))
+            {
+                string headerLine = reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    DateTime dt;
+                    bool success = DateTime.TryParse(values[0], out dt);
+
+                    books.Add (new Book() {
+                        Published = dt,
+                        Title = values[1],
+                        Authors = values[2]
+                    });
+                }
+            }
+            CreateTextFile();
+        }
+        static void CreateTextFile()
+        {
+            string path = @"c:\work\training\stringsce\books.dat";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("| Pub Date    |                       Title | Authors                         |");
+                    sw.WriteLine("| ============|=============================|=================================|");
+                    foreach (var book in books)
+                    {
+                        sw.WriteLine("| " + book.Published.ToString("d MMM yyy") + " | " + book.Title.PadLeft(27) + " | " + book.Authors.PadRight(31) + " |");
+                    }
+                }
+            }
+        }
+    }
+}
